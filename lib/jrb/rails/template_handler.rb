95985f0140428538d42fce9b5e043b690a67460a
Rolf Timmermans <r.timmermans@voormedia.com>
2011-03-30 22:04:58 +0200

    Alias for <<.

--- lib/jrb/rails/template_handler.rb
+++ lib/jrb/rails/template_handler.rb
@@ -5,7 +5,7 @@ module JRB
         def call(template)
           helper = JRB.const_get(template.mime_type.to_sym.to_s.upcase)
 
-          [ "class_eval { def <<(out); @output_buffer << out; end }",
+          [ "class_eval { def <<(out); @output_buffer << out; end; alias_method :write, :<< }",
             "@output_buffer = ActiveSupport::SafeBuffer.new",
             template.source,
             "@output_buffer.to_s" ].join(";")
